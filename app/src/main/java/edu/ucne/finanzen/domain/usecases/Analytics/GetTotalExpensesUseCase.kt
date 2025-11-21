3f9e65123b517cb9500b29a79c43075e276ea810
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-11-21 15:38:16 -0400

    se agregó el caso de uso para obtener el total de gastos

---
+++ app/src/main/java/edu/ucne/finanzen/domain/usecases/Analytics/GetTotalExpensesUseCase.kt
@@ -0,0 +1,10 @@
+package edu.ucne.finanzen.domain.usecases.Analytics
+
+import edu.ucne.finanzen.domain.repository.TransactionRepository
+import javax.inject.Inject
+
+class GetTotalExpensesUseCase @Inject constructor(
+    private val repository: TransactionRepository
+) {
+    suspend operator fun invoke(): Double = repository.getTotalExpenses()
+}
