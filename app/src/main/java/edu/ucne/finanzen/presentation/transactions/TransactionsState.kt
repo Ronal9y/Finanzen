6aff9cb9f61015426327ddbdcb5f06186d11ed11
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-11-21 12:42:28 -0400

    estado de transacciones

---
+++ app/src/main/java/edu/ucne/finanzen/presentation/transactions/TransactionsState.kt
@@ -0,0 +1,10 @@
+package edu.ucne.finanzen.presentation.transactions
+
+import edu.ucne.finanzen.domain.model.Transaction
+
+data class TransactionsState(
+    val transactions: List<Transaction> = emptyList(),
+    val selectedFilter: String = "Todas",
+    val isLoading: Boolean = true,
+    val error: String? = null
+)
