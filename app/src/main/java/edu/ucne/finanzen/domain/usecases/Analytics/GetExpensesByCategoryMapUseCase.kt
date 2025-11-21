06b7fcc5b9238fac0cb6ba34653e4cc09f95ff3f
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-11-21 15:28:17 -0400

    se agregó el caso de uso para obtener gastos por categoría

---
+++ app/src/main/java/edu/ucne/finanzen/domain/usecases/Analytics/GetExpensesByCategoryMapUseCase.kt
@@ -0,0 +1,20 @@
+package edu.ucne.finanzen.domain.usecases.Analytics
+
+import edu.ucne.finanzen.domain.model.TransactionType
+import edu.ucne.finanzen.domain.repository.TransactionRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class GetExpensesByCategoryMapUseCase @Inject constructor(
+    private val repository: TransactionRepository
+) {
+    operator fun invoke(): Flow<Map<String, Double>> =
+        repository.getAllTransactions().map { list ->
+            list
+                .filter { it.type == TransactionType.EXPENSE }
+                .groupBy { it.category.name }
+                .mapValues { (_, txs) -> txs.sumOf { it.amount } }
+                .filterValues { it > 0 } // ← solo con gastos
+        }
+}
