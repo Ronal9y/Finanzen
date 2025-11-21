66a51757e8cc8bad755a8357e9c9faefc0aa39ac
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-11-21 15:22:16 -0400

    se agregó el caso de uso para obtener los datos del dashboard

---
+++ app/src/main/java/edu/ucne/finanzen/domain/usecases/Debts/GetDebtByIdUseCase.kt
@@ -0,0 +1,11 @@
+package edu.ucne.finanzen.domain.usecases.Debts
+
+import edu.ucne.finanzen.domain.model.Debt
+import edu.ucne.finanzen.domain.repository.DebtRepository
+import javax.inject.Inject
+
+class GetDebtByIdUseCase @Inject constructor(
+    private val repository: DebtRepository
+) {
+    suspend operator fun invoke(id: Int): Debt? = repository.getDebtById(id)
+}
