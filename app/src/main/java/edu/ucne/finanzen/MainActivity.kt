f81960ce282b8e60ebc6c1a82b104278484ff79e
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-11-21 16:24:25 -0400

    se agregó la programación de un Worker para verificar finanzas periódicamente y se creó un canal de notificaciones

--- app/src/main/java/edu/ucne/finanzen/MainActivity.kt
+++ app/src/main/java/edu/ucne/finanzen/MainActivity.kt
@@ -1,22 +1,28 @@
 package edu.ucne.finanzen
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.finanzen.Worker.FinanceCheckWorker
+import edu.ucne.finanzen.common.NotificationHelper
+import edu.ucne.finanzen.presentation.navigation.BottomBar
 import edu.ucne.finanzen.presentation.navigation.FinanceNavHost
-import edu.ucne.finanzen.ui.theme.FinanzenTheme
+import edu.ucne.finanzen.presentation.navigation.Screen
+import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 
@@ -24,15 +30,39 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+// Crear canal de notificaciones
+        NotificationHelper.createNotificationChannel(this)
+
+        // Programar Worker
+        val workRequest = PeriodicWorkRequestBuilder<FinanceCheckWorker>(24, TimeUnit.HOURS)
+            .build()
+
+        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+            "finance_check",
+            ExistingPeriodicWorkPolicy.KEEP,
+            workRequest
+        )
+
+        // Solicitar permiso en Android 13+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(Manifest.permission.POST_NOTIFICATIONS), 1001)
+            }
+        }
+
         setContent {
-            FinanzenTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    val navController = rememberNavController()
-                    FinanceNavHost(navController = navController)
-                }
+            val navController = rememberNavController()
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentRoute = navBackStackEntry?.destination?.route
+            val bottomBarVisible = currentRoute != Screen.Welcome::class.qualifiedName &&
+                    currentRoute != Screen.Login::class.qualifiedName
+            Scaffold(
+                bottomBar = { if (bottomBarVisible) BottomBar(navController) }
+            ) { innerPadding ->
+                FinanceNavHost(
+                    navController = navController,
+                    modifier = Modifier.padding(innerPadding)
+                )
             }
         }
     }
