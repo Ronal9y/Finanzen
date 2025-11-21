6f823134d93e06834ce7825f09eac1b97b051477
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-11-21 15:41:00 -0400

    se agregó el caso de uso para obtener el total de deuda restante

---
+++ app/src/main/java/edu/ucne/finanzen/domain/usecases/Debts/GetTotalRemainingDebtUseCase.kt
@@ -0,0 +1,10 @@
+package edu.ucne.finanzen.domain.usecases.Debts
+
+import edu.ucne.finanzen.domain.repository.DebtRepository
+import javax.inject.Inject
+
+class GetTotalRemainingDebtUseCase @Inject constructor(
+    private val repository: DebtRepository
+) {
+    suspend operator fun invoke(): Double = repository.getTotalRemainingDebt()
+}
