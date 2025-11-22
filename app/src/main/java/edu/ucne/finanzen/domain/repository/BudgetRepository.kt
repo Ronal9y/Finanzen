5b39ad6529908c563e63e7687f73d887e4d17b52
ahb647 <58379725+ahb647@users.noreply.github.com>
2025-11-22 01:34:44 -0400

    Se agrego el id del usuario para obtener los presupuestos

--- app/src/main/java/edu/ucne/finanzen/domain/repository/BudgetRepository.kt
+++ app/src/main/java/edu/ucne/finanzen/domain/repository/BudgetRepository.kt
@@ -4,11 +4,11 @@ import edu.ucne.finanzen.domain.model.Budget
 import kotlinx.coroutines.flow.Flow
 
 interface BudgetRepository {
-    fun getAllBudgets(): Flow<List<Budget>>
+    fun getAllBudgets(usuarioId: Int): Flow<List<Budget>>
     suspend fun getBudgetById(id: Int): Budget?
     suspend fun insertBudget(budget: Budget)
     suspend fun updateBudget(budget: Budget)
     suspend fun deleteBudget(budget: Budget)
     suspend fun deleteBudgetById(id: Int)
-    suspend fun getBudgetsCount(): Int
+    suspend fun getBudgetsCount(usuarioId: Int): Int
 }
