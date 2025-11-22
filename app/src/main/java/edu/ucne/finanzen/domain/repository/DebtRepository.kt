11a7cbbfdea1a23661294429288e6e08c4088c69
ahb647 <58379725+ahb647@users.noreply.github.com>
2025-11-22 01:40:05 -0400

    Ajustar el DebtRepository para filtrar por usuarioId
    Se modificaron los métodos del `DebtRepository` para aceptar un `usuarioId` como parámetro. Esto permite filtrar las deudas y los cálculos relacionados por usuario.

--- app/src/main/java/edu/ucne/finanzen/domain/repository/DebtRepository.kt
+++ app/src/main/java/edu/ucne/finanzen/domain/repository/DebtRepository.kt
@@ -4,17 +4,15 @@ import edu.ucne.finanzen.domain.model.Debt
 import kotlinx.coroutines.flow.Flow
 
 interface DebtRepository {
-    fun getAllDebts(): Flow<List<Debt>>
+    fun getAllDebts(usuarioId: Int): Flow<List<Debt>>
     suspend fun getDebtById(id: Int): Debt?
     suspend fun insertDebt(debt: Debt)
     suspend fun updateDebt(debt: Debt)
     suspend fun deleteDebt(debt: Debt)
     suspend fun deleteDebtById(id: Int)
-    suspend fun getDebtsCount(): Int
-    suspend fun getTotalRemainingDebt(): Double
+    suspend fun getDebtsCount(usuarioId: Int): Int
+    suspend fun getTotalRemainingDebt(usuarioId: Int): Double
     suspend fun saveDebt(debt: Debt)
-
-    fun getActiveDebtsCount(): Flow<Int>
-
-    suspend fun getTotalRemainingDebtFlow(): Flow<Double>
+    fun getActiveDebtsCount(usuarioId: Int): Flow<Int>
+    fun getTotalRemainingDebtFlow(usuarioId: Int): Flow<Double>
 }
