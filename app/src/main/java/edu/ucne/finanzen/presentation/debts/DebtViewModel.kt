f9703d63b760be01ccad67fcf48b79d09c33e57b
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-11-28 15:38:42 -0400

    se volvio a usar insert y update

--- app/src/main/java/edu/ucne/finanzen/presentation/debts/DebtViewModel.kt
+++ app/src/main/java/edu/ucne/finanzen/presentation/debts/DebtViewModel.kt
@@ -104,7 +104,7 @@ class DebtViewModel @Inject constructor(
             creationDate = hoy()
         )
 
-        repo.upsertDebt(deudaRenovada)
+        repo.updateDebt(deudaRenovada)
         onEvent(DebtEvent.CerrarDialogoRenovar)
         cargar()
     }
@@ -179,7 +179,7 @@ class DebtViewModel @Inject constructor(
             creationDate = hoy(),
             usuarioId = userId
         )
-        repo.upsertDebt(deudaParaGuardar)
+        repo.insertDebt(deudaParaGuardar)
     }
 
     private fun eliminar(id: Int) = viewModelScope.launch {
@@ -213,7 +213,7 @@ class DebtViewModel @Inject constructor(
             status = if (nuevoRemaining <= 0.0) DebtStatus.PAID else deuda.status
         )
 
-        repo.upsertDebt(deudaActualizada)
+        repo.updateDebt(deudaActualizada)
         cargar()
     }
 
